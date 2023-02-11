f4feca90e6d10c19923120eb0dd17999929cc55e
NigeloYang <yangqiantao@outlook.com>
2023-02-11 10:47:03 +0800

    完成部分基础树算法和排序和搜索算法

---
+++ BaseAlgorithm/Tree/t3.py
@@ -0,0 +1,30 @@
+import TreeNode as trees
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    class Solution:
+        def isSymmetric(self, root):
+            pass
+
+
+if __name__ == "__main__":
+    data = [1, 2, 2, 3, 4, 4, 3]
+    data2 = [1, 2, 2, None, 3, None, 3]
+    tree1 = trees.Tree()
+    tree2 = trees.Tree()
+    
+    tree1.createTree(data)
+    tree1.breadth_travel()
+    print()
+    tree2.createTree(data2)
+    tree2.breadth_travel()
+    # print('\n 验证二叉搜索树结果:', Solution().isValidBST(tree1.root))
+    # print('\n 验证二叉搜索树结果:', Solution().isValidBST(tree2.root))
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# algorithm
+# 介绍
+主要记录初中高级算法
 
-#### 介绍
-主要记录初中高级算法基础学习
+## BaseAlgorithm
+主要内容是记录leetcode中的基础算法学习，主要分为以下几个目录  
+- [Arrar](./BaseAlgorithm/Array): 记录数组相关算法 
+- [String](./BaseAlgorithm/String): 记录字符串相关算法 
+- [Linkedlist](./BaseAlgorithm/String): 记录链表相关算法 
